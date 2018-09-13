1619499a59cf98d65307d2e11698c3f04f8bf350
oscar ponte <opbaquero@esei.uvigo.es>
2018-09-13 09:51:44 +0200

    --> 5 - 6 - 7 - 8 - 9

--- Documents/NetBeansProjects/AEDII/Actividad_1_2018-2019/src/actividad_1_2018/pkg2019/solucion_actividad_1.java
+++ Documents/NetBeansProjects/AEDII/Actividad_1_2018-2019/src/actividad_1_2018/pkg2019/solucion_actividad_1.java
@@ -50,5 +50,77 @@ public class solucion_actividad_1 {
 		else if (n1 > n2) return euclides(n1-n2, n2);
 		else return euclides (n1, n2-n1);
 	}
+        
+        //Ejercicio 5
+        public static boolean palindromo (String cadena){
+            if (cadena.length() == 0 || cadena.length() == 1){
+                return true;
+            }
+            else if (cadena.charAt(0)!= cadena.charAt(cadena.length()-1)){
+                return false;
+            }
+            else return palindromo(cadena.substring(1, cadena.length() - 1));
+            
+        }
+        
+        //Ejercicio 6
+        public static int sumarValoresArray(int[] vector){
+            return sumarValoresArray(vector, vector.length-1);
+        }
+        private static int sumarValoresArray(int[] vector, int fin)
+	{
+		if (fin == 0) return vector[fin];
+                else return vector[fin] + sumarValoresArray(vector, fin - 1);
+	}
+        
+        //Ejercicio 7
+          public static void invertirArray(int[] vector){
+            invertirArray(vector, vector.length-1);
+        } 
+	private static void invertirArray(int[] vector, int fin)
+	{
+		if (fin!= 0){
+			int aux = vector[0];
+			for (int i= 1; i<=fin; i++){
+				vector[i-1] = vector[i];
+			}
+			vector[fin] = aux;
+			invertirArray(vector, fin-1);
+			}
+	}
+        
+        //Ejercicio 8
+          public static int menorArray(int[] vector){
+            return menorArray(vector, vector.length-1, Integer.MAX_VALUE);
+        }
+        public static int menorArray(int[] vector, int indice, int menor){
+            if (indice == 0){
+                if (vector[indice] < menor)
+                    return vector[indice];
+                else
+                    return menor;
+            }
+            else{
+                if (vector[indice] < menor)
+                    return menorArray(vector, indice - 1, vector[indice]);
+                else
+                    return menorArray(vector, indice - 1, menor);
+            }
+        }
+        
+        //Ejercicio 9
+        public static int busquedaBinaria (double [] array, int inicio, int fin, double elemento){
+	    int medio;
+            if (inicio>fin) return -1;
+            else {
+                medio = (fin+inicio)/2;
+                if (array[medio]<elemento)
+                    return busquedaBinaria(array, medio+1, fin, elemento);
+                else if (array[medio]>elemento)
+                        return busquedaBinaria(array, inicio, medio-1, elemento);
+                     else return medio;
+            }
+        }
+        
     
 }
