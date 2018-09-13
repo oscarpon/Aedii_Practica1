af802fee28acbc658d902c78b528e6a5950508ff
oscar ponte <opbaquero@esei.uvigo.es>
2018-09-13 09:22:52 +0200

    -->Factorial

---
+++ Documents/NetBeansProjects/AEDII/Actividad_1_2018-2019/src/actividad_1_2018/pkg2019/solucion_actividad_1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad_1_2018.pkg2019;
+
+/**
+ *
+ * @author oscar
+ */
+public class solucion_actividad_1 {
+    
+    /**
+     * Para los metodos recursivos tenemos que cumplir con unas condiciones, 
+     * Condicion de parada
+     * Llamada recursiva
+     * 
+     */
+    
+    //EJERCICIO 1
+    public static int factorial(int n){
+        if(n == 0) return 1; 
+        else{
+            return n * factorial(n-1);
+        }
+    }
+    
+}
