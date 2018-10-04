e6c55873f1b2aad4f89b85ac72a0816745fa776b
oscar ponte <opbaquero@esei.uvigo.es>
2018-10-04 10:35:06 +0200

    --> 10

--- Documents/NetBeansProjects/AEDII/Actividad4_1819/src/actividad4_1819/Heap.java
+++ Documents/NetBeansProjects/AEDII/Actividad4_1819/src/actividad4_1819/Heap.java
@@ -5,10 +5,22 @@
  */
 package actividad4_1819;
 
+import java.lang.reflect.Array;
+
 /**
  *
  * @author oscar
  */
-public class Heap {
+public class Heap <E extends Comparable<E>>{
+    public static final int CAPACIDAD = 25;
+    private E [] vector;
+    private int numElem;
+    
+    
+    public Heap (Class clase, E superior){
+        numElem = 0;
+        vector = (E[]) Array.newInstance(clase, CAPACIDAD+1);
+	vector[0]= superior;
+    }
     
 }
